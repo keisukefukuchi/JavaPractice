be1a21d40c306ffb45c115607c2d560f620415e1
keisukefukuchi <kekke53530708@gmail.com>
2023-05-30 21:55:34 +0900

    add: ゲーム

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Hero h = new Hero();
+        h.name = "test";
+        h.hp = 100;
+
+        Matango m1 = new Matango();
+        m1.hp = 80;
+        m1.suffix = 'A';
+
+        Matango m2 = new Matango();
+        m2.hp = 50;
+        m2.suffix = 'B';
+
+        h.slip();
+        m1.run();
+        m2.run();
+        h.run();
     }
 }
